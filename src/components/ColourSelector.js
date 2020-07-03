035f723312d493c81d179899ff088f4e4803b979
Cat <ccallcottstevens@gmail.com>
2020-07-03 18:49:25 +1000

    added an event listener on colour change

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -4,9 +4,13 @@ class ColourSelector extends Component {
     render(){
         const { hex } =this.props;
         return (
-            <input type='color' defaultValue={hex} />
+            <input type='color' defaultValue={hex} onChange={(event) => console.log(event)} />
         );
     }
+}
+
+ColourSelector.defaultProps = {
+    hex: "#00ffff"
 }
 
 export default ColourSelector;
